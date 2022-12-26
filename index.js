3fc8ec1ba09f238b7a4b78e68a25e3eed70aeef0
emanikor <87859876+emanikor@users.noreply.github.com>
2022-12-26 15:06:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let display = document.getElementById('display');
-let buttons = array.from(document.getElementByClassName('button'));
+let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map( button => {
-    button.addEventListener('click' ,(e) => {
-       console.log('clicked');
-       console.log(e);
-       console.log( e.target);
-       console.log(e.target.innerText)
+    button.addEventListener('click', (e) => {
+       switch(e.target.innerText){
+        case 'c':
+            display.innerText = '';
+            break;
+        default: 
+           display.innerText+=e.target.innerText;
+       
+       }
     })
 });
